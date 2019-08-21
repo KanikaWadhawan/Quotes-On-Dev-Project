fffadd709715ed431ad834da70743468cfc1259e
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-08-21 12:05:49 -0700

    added simple get request for getting rest api

--- js/qod.js
+++ js/qod.js
@@ -1,11 +1,30 @@
 (function($){
     $(function(){
 
+        //Get request for wp/v2/posts
+        
+        $('#new-quote-button').on('click', function(event){
 
-    // $('body').append('hello cat world');
-    // console.log(qod_api);
 
 
+
+            event.preventDefault();
+
+            $.ajax({
+
+                method: 'get',
+                url: qod_api.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+            }).done(function(data){
+              console.log(data);
+
+
+            }).fail(function(err){
+
+                console.log('error',err);
+            });
+ });
+
+           // Post request for wp/v2/posts
 
 });
 
