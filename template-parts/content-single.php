3b9d78981cb1064f689e8cc8a9b32153f738b4fe
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-08-22 16:29:17 -0700

    worked on styling of single post

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -5,7 +5,11 @@
  * @package QOD_Starter_Theme
  */
 
+ 
+$source = get_post_meta( get_the_ID(), '_qod_quote_source', true);
+$source_url = get_post_meta( get_the_ID(), '_qod_quote_source_url', true);
 ?>
+
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
@@ -13,10 +17,30 @@
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-	</div><!-- .entry-content -->
+		</div><!-- .entry-content -->
+		<div class="entry-meta">
+           <h2 class="entry-title">-<span class="author"><?php the_title(); ?><span><?php if($source && $source_url) : ?><span class="source">, <a href="<?php echo $source_url; ?>"><?php echo $source; ?></a></span>
+           
+		   
+			<?php elseif($source): ?>
+			<span class="source">
+			   <?php echo $source; ?>
+			</span>
+
+			<?php else: ?>
+			<span class="source"></span>
+			<?php endif; ?>
+   </div>
+	
+
+		
+	
 </article><!-- #post-## -->
+<button type="button" id="new-quote-button">Show Me Another!
+		
+		</button>
